871f37201916df7da9677e4e0eb4f9f1b20c9c00
Taufik <taufik@thirdbaseventures.com>
2016-08-27 11:09:55 +0700

    delete unused class

--- app/src/main/java/com/threeosix/exp/shakemotiondetection/MainActivity.java
+++ app/src/main/java/com/threeosix/exp/shakemotiondetection/MainActivity.java
@@ -2,12 +2,75 @@ package com.threeosix.exp.shakemotiondetection;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AccelerometerListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onAccelerationChanged(float x, float y, float z) {
+
+    }
+
+    @Override
+    public void onShake(float force) {
+        // Do your stuff here
+
+        // Called when Motion Detected
+        Toast.makeText(getBaseContext(), "Motion detected",
+                Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Toast.makeText(getBaseContext(), "onResume Accelerometer Started",
+                Toast.LENGTH_SHORT).show();
+
+        //Check device supported Accelerometer senssor or not
+        if (AccelerometerManager.isSupported(this)) {
+
+            //Start Accelerometer Listening
+            AccelerometerManager.startListening(this);
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        //Check device supported Accelerometer senssor or not
+        if (AccelerometerManager.isListening()) {
+
+            //Start Accelerometer Listening
+            AccelerometerManager.stopListening();
+
+            Toast.makeText(getBaseContext(), "onStop Accelerometer Stoped",
+                    Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.i("Sensor", "Service  destroy");
+
+        //Check device supported Accelerometer senssor or not
+        if (AccelerometerManager.isListening()) {
+
+            //Start Accelerometer Listening
+            AccelerometerManager.stopListening();
+
+            Toast.makeText(getBaseContext(), "onDestroy Accelerometer Stoped",
+                    Toast.LENGTH_SHORT).show();
+        }
+
     }
 }
